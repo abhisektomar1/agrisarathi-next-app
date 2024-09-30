9bcf0974f2d99cee261d20c65c91be2430008d3a
abhisektomar1 <abhisektomar1@gmail.com>
2024-09-30 16:38:01 +0530

    know more page slider done

--- app/know-more/page.tsx
+++ app/know-more/page.tsx
@@ -1,47 +1,83 @@
 /* eslint-disable react/no-unescaped-entities */
-import AgrisarathiInfo from '@/components/know-more/Timeline'
-import ComingSoonSection from '@/components/landing/ComingSoon'
-import Image from 'next/image'
-import React from 'react'
+import Slider from "@/components/know-more/Slider";
+import AgrisarathiInfo from "@/components/know-more/Timeline";
+import ComingSoonSection from "@/components/landing/ComingSoon";
+import Marquee from "@/components/magicui/marquee";
+import Image from "next/image";
+import React from "react";
 
 function page() {
   return (
     <>
-    <section className="w-full py-12 md:py-24 lg:py-32 bg-white mx-auto sm:px-20 px-6">
-      <div className="container px-4 md:px-6 mx-auto">
-        <h2 className="text-3xl md:text-5xl font-bold text-center mb-8">
-          Discover <span className="text-green-600">AgriSarathi</span>
-        </h2>
-        <h5 className="text-xl md:text-xl font-normal text-center mb-8">
-        Transforming Agriculture for a Better Tomorrow
-        </h5>
-        <div className="mx-auto py-4">
-          <div className="rounded-lg overflow-hidden mb-8 md:p-20">
-            <Image
-              src="/know-more.png"
-              alt="Team meeting silhouette against sunset"
-              width={600}
-              height={300}
-              className="w-full h-auto object-cover"
-            />
+      <section className="w-full py-12 md:py-24 lg:py-32 bg-white mx-auto sm:px-20 px-6">
+        <div className="container px-4 md:px-6 mx-auto">
+          <h2 className="text-3xl md:text-5xl font-bold text-center mb-8">
+            Discover <span className="text-green-600">AgriSarathi</span>
+          </h2>
+          <h5 className="text-xl md:text-xl font-normal text-center mb-8">
+            Transforming Agriculture for a Better Tomorrow
+          </h5>
+          <div className="mx-auto py-4">
+            <div className="rounded-lg overflow-hidden mb-8 md:p-20">
+              <Image
+                src="/know-more.png"
+                alt="Team meeting silhouette against sunset"
+                width={600}
+                height={300}
+                className="w-full h-auto object-cover"
+              />
+            </div>
+            <p className="text-gray-400 text-sm md:text-base leading-relaxed">
+              AgriSarathi, an indigenously developed, white label platform for
+              all Agri needs of Farmers, FPOs, Buyers and other key
+              stakeholders, draws a parallel with Lord Krishna as the Charioteer
+              to Arjuna in the Indian epic, Mahabharat.
+            </p>
+            <p className="text-gray-400 text-sm md:text-base leading-relaxed mt-4">
+              Likewise, AgriSarathi, through its intelligent congregation of
+              multiple solutions - Climate Smart and Sustainable Farming,
+              Improved Traceability and Democratization of Trade, and Rewards
+              and Recognition, steers the agricultural journey of the Indian
+              Farmer through infusion of digitally smart technologies,
+              optimising resources, enhancing yield and increasing farmers’
+              income.
+            </p>
+            <p className="text-gray-400 text-sm md:text-base leading-relaxed mt-4">
+              Each of AgriSarathi tools is an end-to-end solution while the
+              unified and aggregated AI/ML enabled platform brings together
+              niche offerings of input providers to generate unique data sets
+              for analytical use.
+            </p>
           </div>
-          <p className="text-gray-400 text-sm md:text-base leading-relaxed">
-          AgriSarathi, an indigenously developed, white label platform for all Agri needs of Farmers, FPOs, Buyers and other key stakeholders, draws a parallel with Lord Krishna as the Charioteer to Arjuna in the Indian epic, Mahabharat. 
-          </p>
-          <p className="text-gray-400 text-sm md:text-base leading-relaxed mt-4">
-          Likewise, AgriSarathi, through its intelligent congregation of multiple solutions - Climate Smart and Sustainable Farming, Improved Traceability and Democratization of Trade, and Rewards and Recognition, steers the agricultural journey of the Indian Farmer through infusion of digitally smart technologies, optimising resources, enhancing yield and increasing farmers’ income. 
-          </p>
-          <p className="text-gray-400 text-sm md:text-base leading-relaxed mt-4">
-          Each of AgriSarathi tools is an end-to-end solution while the unified and aggregated AI/ML enabled platform brings together niche offerings of input providers to generate unique data sets for analytical use.
-          </p>
         </div>
-      </div>
-      
-    </section>
-    <AgrisarathiInfo />
-    <ComingSoonSection />
+        <div className="relative overflow-hidden rounded-2xl my-10">
+          <Image
+            src="/drone.png"
+            alt="Hand holding a smartphone with a miniature farm and a drone flying above"
+            width={800}
+            height={400}
+            className="w-full h-auto object-cover"
+          />
+          <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+          <div className="absolute bottom-0 left-0 right-0 p-6 text-white">
+            <div className="flex flex-row items-center justify-center gap-10">
+              <h2 className="text-4xl  md:text-6xl font-bold mb-4">Vision</h2>
+              <div className="bg-black/50 backdrop-blur-sm rounded-lg p-4 max-w-4xl border-white border">
+                <p className="text-sm md:text-xl">
+                  AgriSarathi Is Set Out To Build Intelligent Agri Value Chains
+                  With The Objective Of Bringing The Robust Agricultural
+                  Ecosystem At The Doorstep Of The Farmer
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <AgrisarathiInfo />
+      <Slider />
+      <ComingSoonSection />
     </>
-  )
+  );
 }
 
-export default page
+export default page;
---
+++ components/know-more/Slider.tsx
@@ -0,0 +1,100 @@
+import Image from "next/image";
+import React from "react";
+import Marquee from "../magicui/marquee";
+
+function Slider() {
+  const benefits = [
+    // {
+    //   icon: "/fslider/1.svg",
+    //   title: "Introducing Smart Agriculture Technologies",
+    // },
+    {
+      icon: "/fslider/2.svg",
+      title: "IEase of Access to Information 24/7 through Various Channels",
+    },
+    // {
+    //   icon: "/fslider/3.svg",
+    //   title: "Timely AI-Based Alerts",
+    // },
+    {
+      icon: "/fslider/4.svg",
+      title: "Excellent Blend of Advisory with Real-Time Delivery of Services",
+    },
+    {
+      icon: "/fslider/5.svg",
+      title: "Ensuring Crop Health and Promoting Climate Resilience",
+    },
+    {
+      icon: "/fslider/6.svg",
+      title: "End-to-End Services across the Entire Value Chain",
+    },
+    {
+      icon: "/fslider/7.svg",
+      title: "Enabling Informed Decisions for Maximising Yield",
+    },
+    {
+      icon: "/fslider/8.svg",
+      title:
+        "Ensuring Sustainable Resource Management and Safe Food Production.",
+    },
+    {
+      icon: "/fslider/9.svg",
+      title: "Creating a More Informed and Bonded Farmer/FPO Community",
+    },
+    {
+      icon: "/fslider/10.svg",
+      title:
+        "Encouraging Self-Reliance and Helping Farmers Optimize Resource Usage",
+    },
+    {
+      icon: "/fslider/11.svg",
+      title:
+        "Rewarding Technology Adoption through Various Motivational Schemes.",
+    },
+  ];
+
+  return (
+    <section className="w-full py-12 md:py-24 lg:py-32 bg-white mx-auto sm:px-20 px-6">
+      <div className="relative overflow-hidden rounded-2xl my-10">
+        <Image
+          src="/drone2.png"
+          alt="Hand holding a smartphone with a miniature farm and a drone flying above"
+          width={800}
+          height={400}
+          className="w-full h-auto object-cover"
+        />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
+        <div className="absolute bottom-0 left-0 right-0 p-6 text-white">
+          <div className="relative flex w-full flex-col items-center justify-center overflow-hidden rounded-lg">
+            <div className="flex flex-col justify-start items-center gap-4">
+              <h2 className="text-left text-4xl text-white font-bold">
+                Benefits of Agrisarathi
+              </h2>
+              <Marquee pauseOnHover className="[--duration:20s]">
+                {benefits.map((benefit, index) => (
+                  <div key={index} className="w-[324px] min-h-[114px] px-2 p-4">
+                    <div className="border-white border rounded-lg p-4 flex items-start space-x-4">
+                      <div className="border-white border rounded-lg p-2">
+                        <Image
+                          src={benefit.icon}
+                          alt="features-icons"
+                          width={50}
+                          height={50}
+                        />
+                      </div>
+                      <div>
+                        <h3 className="font-semibold mb-2">{benefit.title}</h3>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </Marquee>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Slider;
